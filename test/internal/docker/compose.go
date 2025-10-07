2300155feca99b450955380ccd485bba08055d13
gdlol <gdlol@users.noreply.github.com>
2025-10-07 15:51:12 +0000

    debugging

--- test/internal/docker/compose.go
+++ test/internal/docker/compose.go
@@ -17,6 +17,9 @@ func ComposeUp(ctx context.Context, composeFilePath string) {
 	defer func() {
 		if r := recover(); r != nil {
 			workspace.RunCtxWithChdir(ctx, composePath, "docker", "compose", "logs", "--no-color")
+			// Debugging
+			workspace.RunCtxWithChdir(ctx, composePath, "docker", "inspect", "--format='{{json .State.Health}}'", "server")
+			panic(r)
 		}
 	}()
 	workspace.RunCtxWithChdir(ctx, composePath, "docker", "compose", "up", "--detach")
